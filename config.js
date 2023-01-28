ec85fc43fb08f7f4956980a4cd8bd004eae1a49c
Skr1pt <44027401+Skr1pt@users.noreply.github.com>
2023-01-28 08:38:20 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,55 @@
+module.exports = {
+    // Configuration for the HTTP API server
+    'http': {
+        'port': 80
+    },
+    // Whether to trust a forwarding proxy's IP (trust X-Forwarded-For)
+    'trust_proxy': false,
+    // List of usernames and passwords for the Steam accounts
+    'logins': [{"user":"axelpr28","pass":"1XdhnjkkC4cyjpu"},{"user":"elderguerrerojc","pass":"tZCDcvJcmCX6R7J"},{"user":"baozi11122","pass":"EIU8zqJ9UiP0PN9"},{"user":"hzghzg1986","pass":"65BlPxmFZsykwF7"},{"user":"victorianez","pass":"qf821YRYi7pES4G"},{"user":"pioter1999","pass":"YJ3lViUVwlvihXv"},{"user":"atom135","pass":"WlMX5jF4WVB3MNg"},{"user":"Thalox","pass":"6K88PT2fP2xwrmP"},{"user":"nra21657","pass":"QlAuZplWJb5TSy9"},{"user":"jmvxx","pass":"3DT9dyMSd6TgMtA"},{"user":"kendebug","pass":"4eOcgjfPX9ydQGL"},{"user":"defnies","pass":"5T9fEU0U1XHN1O1"},{"user":"burge","pass":"587a25l2IG0Eg42"},{"user":"skatet","pass":"GMRo8ZNTCcG1y7W"},{"user":"nus1017","pass":"aMrWYGSQn9pNpDU"},{"user":"bfaurie","pass":"J47ilAyclzMRxgN"},{"user":"david_andreatta","pass":"VDHpC5POf4wsUvg"},{"user":"landerr83","pass":"AtZ5EuFOTkYrDUH"},{"user":"filipm26","pass":"tlOJJiEJlT3ApI0"},{"user":"gurgletears","pass":"2ycTTWz0ObzOfq4"},{"user":"zoszl1","pass":"W4qSndiXPY7tJ26"},{"user":"gaajaa_gj","pass":"WvZTVtc9zsjQC35"},{"user":"rlilly8671","pass":"ifPHTAxnig3gNFm"},{"user":"amit79","pass":"rWIcLhDBZzqO50s"},{"user":"delmuth","pass":"grOayixbkcjbkA2"},{"user":"pokitopelo2002","pass":"ZfWHh7OeQyKCz1K"},{"user":"al_sus","pass":"XP6cZASwvckw9Cs"},{"user":"cazz56","pass":"MOBl8ZGw8m9TGLR"},{"user":"nicolaihorsmark","pass":"ulyeuYbTCL2KflX"},{"user":"jess_jim","pass":"tcShbptuAF5uj8i"},{"user":"silaiceps","pass":"6nerVKcrem5MJHM"},{"user":"chipmaker1966","pass":"XW69nz0Uf3cPDtM"},{"user":"grod3million","pass":"aozQ1uv6NtL7Myb"},{"user":"coolerpower","pass":"Vj6194yb7pH688D"},{"user":"fruitelka","pass":"FcqKtPSq8jVkui5"},{"user":"unh0lymonkey","pass":"Kx2Vvacc29fu7lZ"},{"user":"encome","pass":"muTDRqS4YTBOfUu"},{"user":"andrepfz","pass":"grcm75vXklzGjjp"},{"user":"slim1sam","pass":"d7kUdKv7wVuqVpr"},{"user":"kubabuba13","pass":"9sH0QCLr1rGkGrK"},{"user":"p2133","pass":"1hrM3Vs483eVGK9"},{"user":"ulisestarino","pass":"u4BqxLFC9bE8LUN"},{"user":"ewok13_9","pass":"3gVAabL7mfWsUCK"},{"user":"yaswinrai","pass":"FftCdSUWjlF5ZxZ"},{"user":"Kawzified","pass":"r8ByGEuVai1ptxZ"},{"user":"renatojc83","pass":"tbEQ4Gx4HYIcLwF"},{"user":"toshi47","pass":"3QL18pgEzcY0KlI"},{"user":"davin315","pass":"3jGwFPcvI8JoJ2y"},{"user":"ga03176","pass":"AWRa1dYL3eE7cbE"},{"user":"lumgey1997","pass":"SVyP7azYHNMK0A1"},{"user":"carygirl21","pass":"Qtmm2w3SRFI27VL"},{"user":"denyman1","pass":"L7voyjV0mnjKoVp"},{"user":"zolrock","pass":"QdZ1yONKlUGPkzn"},{"user":"skullman2010","pass":"lmob4uoSfT32HaK"},{"user":"mini_t_93","pass":"gGgCr7SYxqOR3Al"},{"user":"a214465","pass":"zlChyvD5sWvGu5O"},{"user":"crossmuckel3","pass":"7tHsF3zDBl02ghC"},{"user":"asbman","pass":"02K5cbXCcr2PFkX"},{"user":"dsfsdf85","pass":"qT7IynP38lWicvw"},{"user":"jayman071","pass":"1PE0yWHhi7oyJnc"},{"user":"mars_vandal","pass":"odXyDx1gLUSJtuo"},{"user":"smokarock","pass":"cFCkdOW6YpbjLeH"},{"user":"griffiaa","pass":"HjU6G3d3mMsSQch"},{"user":"vetinh_tv2003","pass":"SMXDD12Lvv4CMOp"},{"user":"mlfather","pass":"AJKjv9xIwWwyB4u"},{"user":"nurullaev90","pass":"UJJAHOVchXHTA2H"},{"user":"nicrinho","pass":"qvwYyz3EHKSTjJc"},{"user":"manynya","pass":"PnvHtwY0Mzl7gQb"},{"user":"Indria22","pass":"fr7semdqz0gM46z"},{"user":"xomytow","pass":"R2JuBYp83kXFbmo"},{"user":"mmehranzadeh","pass":"epHdRNNjpHP0sKF"},{"user":"kt0709","pass":"KKPjBpG5iXZDHwF"},{"user":"v1cth0r","pass":"YR1PAODCSH7xVWl"},{"user":"clad22","pass":"RzCs2wn1agAX1oJ"},{"user":"saifur10","pass":"sOyM4CfmwdiMhzL"},{"user":"vaggos2285","pass":"3HmOAONYwmtLFSW"},{"user":"xpois3","pass":"xTajpLQokbg9VrQ"},{"user":"hennes80","pass":"gNKW3StXPiurDbh"},{"user":"selotrepus","pass":"OB4B1V5GBEGxpLl"},{"user":"Guilhemal","pass":"02T7Y7mat2YRuE0"},{"user":"montek1","pass":"43NrcWN7RTxN3ri"},{"user":"erik_skilbred","pass":"2QC0WMiGubhgGOn"},{"user":"zacka427","pass":"ryW91MIw6r7mic9"},{"user":"sanjeyk1","pass":"fLkIEz4R2EDH1RK"},{"user":"zaheerybl","pass":"Gh5IweZoxVnbNgy"},{"user":"kishikiryuu","pass":"PbMOSJY761iltoT"},{"user":"our_salah11","pass":"XdvVVYva4ASqQNa"},{"user":"oxotnic_y","pass":"EvbMilJf9mMLby7"},{"user":"vitya055","pass":"9MB6u1kOJ5HnGhb"},{"user":"dxgupta","pass":"5rMgtu9J6krEYJT"},{"user":"airforce1947","pass":"X12f1GtN9N9L788"},{"user":"zurnaist","pass":"auG1Q4Xk2zxaEiI"},{"user":"badboy18","pass":"XQorMtHd74enCBK"},{"user":"piotrboj","pass":"IaPkp59pOmrXiKf"},{"user":"niktemnov","pass":"xK5u8gpv7wXWP2g"},{"user":"roghert","pass":"u1jneRX4w09cSio"},{"user":"tjiyong","pass":"cCxmW35neJfZBnu"},{"user":"leny2543","pass":"fyQpwPh19bALFnW"},{"user":"deiosampa","pass":"khiZ52rNE6CXdaw"},{"user":"szabopeti94","pass":"pZNlttVSwzPb8JK"},{"user":"athar_nafis","pass":"0eGu13ORGQ5zJmF"},{"user":"oedi59","pass":"uLlIwSI4j71zHM3"},{"user":"iceyu2058","pass":"4ymTLOw3ZDxXZOt"},{"user":"Bayuugan","pass":"8D3zxuCgMhQ7OEe"},{"user":"janicklarsen","pass":"1wldO4BNRMXT82W"},{"user":"jcsmp1lb1905","pass":"LJNf3c2hM3laqyr"},{"user":"puhterodactyl","pass":"o72ug4N5J1TYPXj"},{"user":"ccxccq","pass":"T8HmtbHRictz7Bt"},{"user":"MrFantasticMan","pass":"9rzmrLJsCyBFC59"},{"user":"gawlew","pass":"RO8mqsWr0LOxx7p"},{"user":"moki521","pass":"56zigaZO2scUwlO"},{"user":"thedeadhead04","pass":"FE7MfVd6Q8nlNcM"},{"user":"erop20021","pass":"IkYoIpLI6UKFiyh"},{"user":"nima6m","pass":"TvtmQsnDC3Js4Y6"},{"user":"xuyao5471","pass":"s4Vlg0OV5ld3R60"},{"user":"orange9640","pass":"oGHRaD3BDnyC0uf"},{"user":"saumilgogri","pass":"0y0Kzgf3PyPWurj"},{"user":"riyyon","pass":"UstE0KIomXktPay"},{"user":"peipeilin","pass":"OOtU2U6hRWMq0qS"},{"user":"shugger","pass":"2qr7PrdkKhB3lEY"},{"user":"zabalx","pass":"PR1xLEslWa6fGyn"},{"user":"fshingo","pass":"7AUcxCsYWZqHDMR"},{"user":"realwoodenboy","pass":"rXKefmjbWtWU0Jr"},{"user":"technojrg","pass":"5TJaUQgxWaoFOfJ"},{"user":"clipso60","pass":"J0ktgdWLTOQmTCc"},{"user":"dudumaispop","pass":"nzSkXQtVWlC51qy"},{"user":"ikedraft","pass":"VHHmePM6mu9calV"},{"user":"tictic87ct","pass":"xDDqgf8MGD4ABHb"},{"user":"almunpdam","pass":"Dlnv3trHEApA3MQ"},{"user":"chuggawh","pass":"ahgFHQKAICiEF9x"},{"user":"giorgosvr456","pass":"uW6gTUbmKyFsmcs"},{"user":"kengoso","pass":"xmAdGwWfP6yFxec"},{"user":"dfsonidero","pass":"tPre0D3BUIIb7OZ"},{"user":"biggangji","pass":"AnUrB9OX2QYdU1V"},{"user":"matze09x","pass":"9ramK57tnz4aHcb"},{"user":"kirik2211","pass":"Gk6olNeaVXfxCx6"},{"user":"furkanunluu","pass":"kPAyMbHEDNk4Pxe"},{"user":"mosainy","pass":"5sAhZy1b0NWE8pj"},{"user":"nikolas3535","pass":"9ONjLyIS0Noz2SZ"},{"user":"metal412002","pass":"V3nihxoSElm4ovK"},{"user":"bones790175","pass":"RSUbkPiMsmi6WUV"},{"user":"crohog","pass":"VUGFm71ZdN2A4xV"},{"user":"jianfmiranda","pass":"214BFkyoLI6Tnty"},{"user":"glebpresentreal","pass":"hgE6Y9gCiDLuAPI"},{"user":"starlightjose","pass":"FeZocHnO7BmfEJ9"},{"user":"yqhloveyou","pass":"TdPDPTF9YL0JPkW"},{"user":"andersontana","pass":"oRv2ea9v9iQGsCn"},{"user":"matthewjasondau","pass":"IbdJ0mz5eB11FKw"},{"user":"miamikidd305","pass":"RBwO69l0YNLt94S"},{"user":"chenpowei1","pass":"epNJT0XXFwihnDd"}],
+    // Bot settings
+    'bot_settings': {
+        // Amount of attempts for each request to Valve
+        'max_attempts': 1,
+        // Amount of milliseconds to wait between subsequent requests to Valve (per bot)
+        'request_delay': 1100,
+        // Amount of milliseconds to wait until a request to Valve is timed out
+        'request_ttl': 2000,
+        // OPTIONAL: Settings for Steam User (https://github.com/DoctorMcKay/node-steam-user#options-)
+        'steam_user': {}
+    },
+    // Origins allowed to connect to the HTTP/HTTPS API
+    'allowed_origins': [
+        'http://example.com',
+        'https://example.com',
+        'chrome-extension://jjicbefpemnphinccgikpdaagjebbnhg',
+        'http://steamcommunity.com',
+        'https://steamcommunity.com'
+    ],
+    // Origins allowed to connect to the HTTP/HTTPS API with Regex
+    'allowed_regex_origins': [
+        'https://.*\\.steamcommunity\\.com'
+    ],
+    // Optionally configure a global rate limit across all endpoints
+    'rate_limit': {
+        'enable': false,
+        'window_ms': 60 * 60 * 1000, // 60 min
+        'max': 10000
+    },
+    // Logging Level (error, warn, info, verbose, debug, silly)
+    'logLevel': 'debug',
+    // Max amount of simultaneous requests from the same IP  (incl. WS and HTTP/HTTPS), -1 for unlimited
+    'max_simultaneous_requests': 1,
+    // Bool to enable game file updates from the SteamDB Github tracker (updated item definitions, images, names)
+    'enable_game_file_updates': true,
+    // Amount of seconds to wait between updating game files (0 = No Interval Updates)
+    'game_files_update_interval': 3600,
+    // Postgres connection string to store results in (ex. postgres://user:pass@127.0.0.1:5432/postgres?sslmode=disable)
+    'database_url': '',
+    // OPTIONAL: Enable bulk inserts, may improve performance with many requests
+    'enable_bulk_inserts': false,
+    // OPTIONAL: Key by the caller to allow inserting price information, required to use the feature
+    'price_key': '',
+    // OPTIONAL: Key by the caller to allow placing bulk searches
+    'bulk_key': '',
+};
